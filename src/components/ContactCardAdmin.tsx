c25792ca63de725b6c81a644e9fde31e134483f1
patrickariola <patrickariola172@gmail.com>
2024-11-05 14:54:11 -1000

    contacts table finished in 72 minutes.
    First attempt

---
+++ src/components/ContactCardAdmin.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+// import { Contact } from '@/lib/validationSchemas';
+import { Contact } from '@prisma/client';
+import { Card, Image } from 'react-bootstrap';
+
+/* Renders a single row in the List Stuff table. See list/page.tsx. */
+const ContactCardAdmin = ({ contact }: { contact: Contact }) => (
+  <Card>
+    <Card.Header>
+      <Image src={contact.image} width={75} />
+      <Card.Title>
+        {contact.firstName}
+        &nbsp;
+        {contact.lastName}
+      </Card.Title>
+      <Card.Subtitle>{contact.address}</Card.Subtitle>
+    </Card.Header>
+    <Card.Body>
+      <Card.Text>{contact.description}</Card.Text>
+      <p className="blockquote-footer">{contact.owner}</p>
+    </Card.Body>
+  </Card>
+);
+
+export default ContactCardAdmin;
